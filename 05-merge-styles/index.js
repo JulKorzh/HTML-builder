10fe37218b330251504e0ac6cf1d69a79204ac36
JulKorzh <julkorzh1@gmail.com>
2024-01-22 01:04:30 +0300

    feat: done 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+
+const stylesFolder = path.resolve(__dirname, 'styles');
+const bundleFile = path.resolve(__dirname, 'project-dist', 'bundle.css')
+const output = fs.createWriteStream(bundleFile);
+
+fs.truncate(bundleFile, (err) => {
+  if (err) console.log(err);
+})
+
+fs.readdir(stylesFolder, { withFileTypes: true }, (err, files) => {
+  if (err) console.log(err);
+
+  files
+    .filter((v) => v.isFile())
+    .forEach((file) => {
+      const fileExtend = path.extname(file.name);
+      if (fileExtend === '.css') {
+        const readableStream = fs.createReadStream(
+          path.resolve(stylesFolder, file.name),
+        );
+
+        let data = '';
+        readableStream.on('data', (chunk) => (data += chunk));
+        readableStream.on('end', () => {
+          data += '\n';
+          output.write(data);
+        });
+        readableStream.on('error', (error) =>
+          console.log('Error', error.message),
+        );
+      }
+    });
+});
